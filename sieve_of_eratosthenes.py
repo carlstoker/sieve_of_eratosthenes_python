e16269e4fd9d7e5de8a1228a9d44d0d1dadde015
carlstoker <40079934+carlstoker@users.noreply.github.com>
2020-06-26 18:29:20 -0400

    Optimization. Better arg handling

--- sieve_of_eratosthenes.py
+++ sieve_of_eratosthenes.py
@@ -2,18 +2,21 @@
 """Sieve of Eratosthenes"""
 from sys import argv
 
+
 def prime_sieve(n):
     sieve = [True] * n
     sieve[0:2] = [False] * 2
 
     for i in range(2, int(n ** 0.5) + 1):
-        pointer = i * 2
-        while pointer < n:
-            sieve[pointer] = False
-            pointer += i
+        if sieve[i]:
+            pointer = i ** 2
+            while pointer < n:
+                sieve[pointer] = False
+                pointer += i
 
     return [i for i in range(len(sieve)) if sieve[i]]
 
 
-primes = prime_sieve(int(argv[1]))
-print('{} primes found through {}: {}'.format(len(primes), argv[1], primes))
+for arg in argv[1:]:
+    primes = prime_sieve(int(arg))
+    print('{0:,} primes found through {1:,}: {2}'.format(len(primes), int(arg), primes))
